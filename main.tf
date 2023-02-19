81c161c916814e6c1cb5190d938a52e29ea9d5f8
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2023-02-19 06:15:43 +0100

    vpc

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = eu-west-2
 }
 
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
@@ -11,4 +11,14 @@ resource "aws_vpc" "vpc" {
     "name" = "vpc_akin"
   }
 
+}
+
+resource "aws_subnet" "cba_public_01" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "eu-west-2a"
+  tags = {
+    Name = "ApachePublicSubnet_01"
+  }
 }
