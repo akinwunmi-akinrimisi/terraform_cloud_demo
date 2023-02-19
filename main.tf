4fbd26875402f5fc3ea6335e33a283cce3f07a50
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2023-02-19 06:27:02 +0100

    vpc

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
-  instance_tenancy = default
+  instance_tenancy = "default"
   tags = {
     "name" = "vpc_akin"
   }
